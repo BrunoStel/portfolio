f91a278a7b17a5e90766ab2a6d0afe52eede7fcf
Bruno Stelmastchuk <brunostel@gmail.com>
2021-10-20 12:00:54 -0300

    Adicionando arquivos

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+
+
+document.getElementById("threeline-icon").onclick = function(){
+	var x =  document.getElementById("navigation");
+
+	if (x.className === "navigation") {
+		x.className += " menujs";
+		document.getElementById("threeline-icon").innerHTML = "&Cross;";
+
+	} else {
+		x.className = "navigation";
+		document.getElementById("threeline-icon").innerHTML = "&#9776;";
+
+	}
+}
+
